0ea5c2ae947f6537bd5a7ec646cba0b26541815a
Michał Nożkiewicz <michno513@gmail.com>
2024-03-24 04:32:47 +0100

    Something

--- src/character/TestPlayer.gd
+++ src/character/TestPlayer.gd
@@ -22,8 +22,11 @@ func _physics_process(delta):
 			velocity.y += gravity * delta * (MULTIPLICATOR ** 2)
 
 		# Handle jump.
-		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		if Input.is_action_just_pressed("Jump") and is_on_floor():
 			velocity.y = JUMP_VELOCITY * MULTIPLICATOR 
+		
+		if Input.is_action_just_pressed("Attack") and is_on_floor():
+			anim.play("Attack") 
 
 		# Get the input direction and handle the movement/deceleration.
 		# As good practice, you should replace UI actions with custom gameplay actions.
