4fa2f6bc5efce5bfba4c4452ef721935ce7f7c26
Spontaniczny <k2002kuba@gmail.com>
2024-03-24 13:28:38 +0100

    Added intro

---
+++ src/intro/intro.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$AnimationPlayer.play("Fade_in")
+	await get_tree().create_timer(6).timeout
+	$AnimationPlayer.play("Fade_out")
+	await get_tree().create_timer(3).timeout
+	get_tree().change_scene_to_file("res://src/game_management/MultiplayerController.tscn")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
