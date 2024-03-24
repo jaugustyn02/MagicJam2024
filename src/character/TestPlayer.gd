4534c18a43e02c7bde7882d48c13a871d7f9f370
noarkhh <jakub.pryc@gmail.com>
2024-03-24 12:41:15 +0100

    Sword now working

--- src/character/TestPlayer.gd
+++ src/character/TestPlayer.gd
@@ -2,15 +2,21 @@ extends CharacterBody2D
 
 
 const SPEED = 400.0
-const JUMP_VELOCITY = -575.0
+const JUMP_VELOCITY = -800.0
 const INITIAL_LIFETIME = 180.0
-
-var PlayerID: int
+const SWORD_DAMAGE = 10.0
 
 @export var lifetime: float = INITIAL_LIFETIME
+
+var lifetime2: float = INITIAL_LIFETIME
+
 var time_multiplier: float = 1.0
-@onready var active_attack_hitbox = $AttackHitboxRight
 var attack: float = 0
+var PlayerID: int
+var is_attacking: bool = false
+var is_attack_hitbox_active: bool = false
+var pixel_hack_stage: int = 0
+var locked_attack_direction: int = 0
 
 @onready var anim = get_node("AnimationPlayer")
 
@@ -22,33 +28,34 @@ func _ready():
 	PlayerID = multiplayer.get_unique_id()
 	
 	$PlayerHitbox.monitoring = true
-	$AttackHitboxLeft.monitoring = true
-	$AttackHitboxRight.monitoring = true
+	$AttackHitbox.collision_layer = 0
+	$AttackHitbox.collision_mask = 0
+	
 func play_anim(animation):
-	anim.play(animation)
+	if animation != null:
+		anim.play(animation)
 
 func _physics_process(delta):
 	if $MultiplayerSynchronizer.get_multiplayer_authority() != PlayerID:
 		return
 		
 	lifetime -= delta * time_multiplier
+	lifetime2 -= delta * time_multiplier
 	var direction = Input.get_axis("Left", "Right")
 		
 	if not is_on_floor():
 		velocity.y += gravity * delta * (time_multiplier ** 2)
 		
-	if direction == -1:
-		get_node("AnimatedSprite2D").flip_h = true
-		active_attack_hitbox = $AttackHitboxLeft
-	elif direction == 1:
-		get_node("AnimatedSprite2D").flip_h = false
-		active_attack_hitbox = $AttackHitboxRight
-		
+	if !is_attacking:
+		change_direction(direction)
+	
 	# Handle jump.
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY * time_multiplier 
 
-	if Input.is_action_just_pressed("Attack") and is_on_floor():
+	if Input.is_action_just_pressed("Attack") and !is_attacking:
+		change_direction(-1 if (get_local_mouse_position().x < 0) else 1)
+		is_attacking = true
 		anim.play("Attack")
 		
 	# Get the input direction and handle the movement/deceleration.
@@ -59,14 +66,29 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	
 	var animation = "Idle"
-	if velocity.y != 0:
+	if is_attacking and anim.is_playing():
+		if anim.current_animation_position > 0.2:
+			if pixel_hack_stage == 0:
+				$AttackHitbox.position.x += 1
+				pixel_hack_stage = 1
+			elif pixel_hack_stage == 1:
+				$AttackHitbox.position.x -= 1
+				pixel_hack_stage = 2
+			$AttackHitbox.collision_layer = 1
+		animation = null
+		is_attacking = true
+	elif velocity.y != 0:
+		is_attacking = false
+		$AttackHitbox.collision_layer = 0
+		pixel_hack_stage = 0
 		if velocity.y < 0:
 			animation = "Jump"
 		else:
 			animation = "Fall"
-	elif Input.is_action_pressed("Attack"):
-		animation = "Attack"
 	else:
+		is_attacking = false
+		$AttackHitbox.collision_layer = 0
+		pixel_hack_stage = 0
 		if velocity.x == 0:
 			animation = "Idle"
 		else:
@@ -74,7 +96,19 @@ func _physics_process(delta):
 	
 	play_anim(animation)
 	move_and_slide()
-
+	
+func change_direction(direction):
+	if direction == -1:
+		get_node("AnimatedSprite2D").flip_h = true
+		$AttackHitbox.scale.x = -1
+	elif direction == 1:
+		get_node("AnimatedSprite2D").flip_h = false
+		$AttackHitbox.scale.x = 1
+	
 func on_time_multiplier_changed(new_time_multiplier):
 	time_multiplier = new_time_multiplier
 	anim.speed_scale = time_multiplier
+
+func _on_player_hitbox_area_entered(area):
+	if area != $AttackHitbox:
+		lifetime -= SWORD_DAMAGE
