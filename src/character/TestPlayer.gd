0375c9955e6fb51ea7ff0422de985b5a285cfdad
noarkhh <jakub.pryc@gmail.com>
2024-03-24 04:30:03 +0100

    Integrate animated player with gears of time

--- src/character/TestPlayer.gd
+++ src/character/TestPlayer.gd
@@ -3,9 +3,11 @@ extends CharacterBody2D
 
 const SPEED = 400.0
 const JUMP_VELOCITY = -575.0
-const MULTIPLICATOR = 1
+
 
 var PlayerID: int
+
+var time_multiplier: float = 1.0
 
 @onready var anim = get_node("AnimationPlayer")
 
@@ -17,37 +19,42 @@ func _ready():
 	PlayerID = multiplayer.get_unique_id()
 
 func _physics_process(delta):
-	if $MultiplayerSynchronizer.get_multiplayer_authority() == PlayerID:
-		if not is_on_floor():
-			velocity.y += gravity * delta * (MULTIPLICATOR ** 2)
-
-		# Handle jump.
-		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-			velocity.y = JUMP_VELOCITY * MULTIPLICATOR 
-
-		# Get the input direction and handle the movement/deceleration.
-		# As good practice, you should replace UI actions with custom gameplay actions.
-		var direction = Input.get_axis("Left", "Right")
-		if direction:
-			velocity.x = direction * SPEED * MULTIPLICATOR
-		else:
-			velocity.x = move_toward(velocity.x, 0, SPEED)
-			anim.play("Idle")
+	if $MultiplayerSynchronizer.get_multiplayer_authority() != PlayerID:
+		return
 		
-		if direction == -1:
-			get_node("AnimatedSprite2D").flip_h = true
-		elif direction == 1:
-			get_node("AnimatedSprite2D").flip_h = false
-			
-		if velocity.y != 0:
-			if velocity.y < 0:
-				anim.play("Jump")
-			else:
-				anim.play("Fall")
+	if not is_on_floor():
+		velocity.y += gravity * delta * (time_multiplier ** 2)
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY * time_multiplier 
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("Left", "Right")
+	if direction:
+		velocity.x = direction * SPEED * time_multiplier
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		anim.play("Idle")
+	
+	if direction == -1:
+		get_node("AnimatedSprite2D").flip_h = true
+	elif direction == 1:
+		get_node("AnimatedSprite2D").flip_h = false
+		
+	if velocity.y != 0:
+		if velocity.y < 0:
+			anim.play("Jump")
 		else:
-			if velocity.x == 0:
-				anim.play("Idle")
-			else:
-				anim.play("Run")
-				
-		move_and_slide()
+			anim.play("Fall")
+	else:
+		if velocity.x == 0:
+			anim.play("Idle")
+		else:
+			anim.play("Run")
+			
+	move_and_slide()
+
+func on_time_multiplier_changed(new_time_multiplier):
+	time_multiplier = new_time_multiplier
