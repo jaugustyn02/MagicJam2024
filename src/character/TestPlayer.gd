8828a79cd8adadad2110c0862382729142a2541d
Michał Nożkiewicz <michno513@gmail.com>
2024-03-24 03:12:43 +0100

    Basic animations

--- src/character/TestPlayer.gd
+++ src/character/TestPlayer.gd
@@ -6,6 +6,8 @@ const JUMP_VELOCITY = -400.0
 
 var PlayerID: int
 
+@onready var anim = get_node("AnimationPlayer")
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -17,17 +19,33 @@ func _physics_process(delta):
 	if $MultiplayerSynchronizer.get_multiplayer_authority() == PlayerID:
 		if not is_on_floor():
 			velocity.y += gravity * delta
-
 		# Handle jump.
 		if Input.is_action_just_pressed("Jump") and is_on_floor():
 			velocity.y = JUMP_VELOCITY
-
 		# Get the input direction and handle the movement/deceleration.
 		# As good practice, you should replace UI actions with custom gameplay actions.
 		var direction = Input.get_axis("Left", "Right")
 		if direction:
 			velocity.x = direction * SPEED
+			anim.play("Run")
 		else:
 			velocity.x = move_toward(velocity.x, 0, SPEED)
-
+			anim.play("Idle")
+		
+		if direction == -1:
+			get_node("AnimatedSprite2D").flip_h = true
+		elif direction == 1:
+			get_node("AnimatedSprite2D").flip_h = false
+			
+		if velocity.y != 0:
+			if velocity.y < 0:
+				anim.play("Jump")
+			else:
+				anim.play("Fall")
+		else:
+			if velocity.x == 0:
+				anim.play("Idle")
+			else:
+				anim.play("Run")
+				
 		move_and_slide()
